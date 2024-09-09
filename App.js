0591dfff97e7f14c840ecfd7c64f50b9aa71b1fc
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-09-09 17:55:06 -0300

    feat: started

--- App.js
+++ App.js
@@ -1,21 +1,147 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+       {/*ScrollView deixa fazer scroll caso os items passem do tamanho da tela*/}
+      <View style={styles.container}>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.taskContainer}>
+          {/*In react native you have to give style to the Text for color. */}
+          <Text style={styles.textColor}>
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  taskContainer: {
+    color: "blue",
+    margin: 20,
+    padding: 20,
+    backgroundColor: "red",
+    borderRadius: 5,
+  },
+  textColor: {
+    color: "white",
   },
 });
+
+
+/* 
+
+REACT NATIVE:
+
+Part 1: 5min
+https://www.youtube.com/watch?v=0kL6nhutjQ8&ab_channel=MadeWithMatt
+Part 2:
+https://www.youtube.com/watch?v=00HFzh3w1B8&ab_channel=MadeWithMatt
+
+ANOTHER TO DO:
+https://www.youtube.com/watch?v=cmXZNGXjXPM&ab_channel=TheAwesomeDev
+
+
+*/
